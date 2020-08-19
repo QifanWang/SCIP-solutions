7fc9679c35564f62ebe4ebca87a9b4ea6ed8e212
QifanWang <302755067@qq.com>
2020-08-19 22:42:11 +0800

    exercise 1.1~1.5

---
+++ 1.1/ex1.3.scm
@@ -0,0 +1,7 @@
+(define (foo x y z)
+  (cond
+   ((and (>= x z) (>= y z)) (+ (* x x) (* y y)))
+   ((and (>= y x) (>= z x)) (+ (* y y) (* z z)))
+   ((and (>= z y) (>= x y)) (+ (* z z) (* x x))))
+  )
+
